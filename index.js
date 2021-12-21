73b51f80f8d465f31e43c6a0d701461b90e339a7
erdem333 <erdemguengoer@googlemail.com>
2021-12-21 02:42:54 +0100

    added advert detail scrap

--- index.js
+++ index.js
@@ -1,141 +1,222 @@
-const _ = require('lodash');
-const puppeteer = require('puppeteer');
+const _ = require("lodash");
+const puppeteer = require("puppeteer");
 
 let scrapeList = async () => {
-    browser = await puppeteer.launch({ headless: false });
+  let browser = await puppeteer.launch({ headless: true });
 
-    const page = await browser.newPage();
+  let page = await browser.newPage();
 
-    await page.goto('https://www.immowelt.de/liste/wallerfangen/haeuser/kaufen?d=true&r=10');
+  await page.goto(
+    "https://www.immowelt.de/liste/wallerfangen/haeuser/kaufen?d=true&r=10"
+  );
 
-    let paginationButtons = await page.$$('.Button-c3851.secondary-47144.navNumberButton-d264f');
+  let paginationButtons = await page.$$(
+    ".Button-c3851.secondary-47144.navNumberButton-d264f"
+  );
 
-    let allItems = await page.evaluate(() => {
-        const searchListElement = document.querySelector('div[data-test="searchlist"]');
-        const searchItemsElements = searchListElement.querySelectorAll('div[class*="EstateItem"]')
+  let allItems = await page.evaluate(() => {
+    const searchListElement = document.querySelector(
+      'div[data-test="searchlist"]'
+    );
+    const searchItemsElements = searchListElement.querySelectorAll(
+      'div[class*="EstateItem"]'
+    );
 
-        let items = [];
+    let items = [];
 
-        searchItemsElements.forEach((item) => {
-            let aTag = item.querySelector('a')
-            items.push({
-                href: aTag.href,
-                id: aTag.id
-            })
-        })
-
-        return items;
+    searchItemsElements.forEach((item) => {
+      let aTag = item.querySelector("a");
+      items.push({
+        href: aTag.href,
+        id: aTag.id,
+      });
     });
 
+    return items;
+  });
 
-    for (let link of paginationButtons){
-        await Promise.all([
-            page.waitForNavigation(),
-            link.click()
-        ]);
+  for (let link of paginationButtons) {
+    await Promise.all([page.waitForNavigation(), link.click()]);
 
-        let evalResponse = await page.evaluate(() => {
-            const searchListElement = document.querySelector('div[data-test="searchlist"]');
-            const searchItemsElements = searchListElement.querySelectorAll('div[class*="EstateItem"]')
-    
-            let items = [];
-    
-            searchItemsElements.forEach((item) => {
-                let aTag = item.querySelector('a')
-                items.push({
-                    href: aTag.href,
-                    id: aTag.id
-                })
-            })
-    
-            return items;
+    let evalResponse = await page.evaluate(() => {
+      const searchListElement = document.querySelector(
+        'div[data-test="searchlist"]'
+      );
+      const searchItemsElements = searchListElement.querySelectorAll(
+        'div[class*="EstateItem"]'
+      );
+
+      let items = [];
+
+      searchItemsElements.forEach((item) => {
+        let aTag = item.querySelector("a");
+        items.push({
+          href: aTag.href,
+          id: aTag.id,
         });
-        allItems.push(...evalResponse);
-    }
+      });
 
-    await browser.close();
+      return items;
+    });
+    allItems.push(...evalResponse);
+  }
 
-    return allItems;
+  await browser.close();
+
+  return allItems;
 };
 
-scrapeInfoPage = async () => {
+scrapeInfoPage = async (list) => {
+  let browser = await puppeteer.launch({ headless: false, devtools: true });
 
+  let page = await browser.newPage();
+  let advertObject;
+  for (let i = 0; i < list.length; i++) {
+    let advert = list[i];
+
+    console.log("advert Link", advert.href);
+    await page.goto(advert.href);
+
+    result = await page
+      .evaluate(() => {
+        const tmp = document.querySelectorAll(".has-font-300");
+        if (tmp[0]) {
+          const preis = tmp[0].querySelectorAll("strong")[0] || "";
+          const wohnFlaeche = tmp[1].innerText || "";
+          const zimmer = tmp[2].innerText || "";
+          const grundFlaeche = tmp[3].innerText || "";
+
+          advertObject = {
+            preis: preis.innerText,
+            wohnFlaeche: wohnFlaeche,
+            zimmer: zimmer,
+            grundFlaeche: grundFlaeche,
+          };
+          console.log("#1 advertObject", advertObject);
+          return advertObject;
+        } else {
+          return;
+        }
+      })
+      .then(async (advertObject) => {
+        let elHandle = await page.$x("//*[@id='aUebersicht']/h1");
+        if (elHandle && elHandle.length > 0) {
+          const headerText = await page.evaluate(
+            (el) => el.textContent || "",
+            elHandle[0]
+          );
+
+          advertObject["headerText"] = headerText;
+
+          return advertObject;
+        } else {
+          return;
+        }
+      })
+      .then(async (advertObject) => {
+        let elHandleList = await page.$x(
+          "//*[@id='mainGallery']/div[*]/div[*]/app-media-item/picture/img"
+        );
+        if (elHandleList && elHandleList.length > 0) {
+          let picsArray = [];
+
+          await new Promise((resolve, reject) => {
+            elHandleList.forEach(async (elemHandle, i) => {
+              const img = await page.evaluate((imgTag) => {
+                return {
+                  url: imgTag.src,
+                  title: imgTag["dataset"].src || imgTag.title,
+                };
+              }, elemHandle);
+
+              picsArray.push(img);
+              if (i === elHandleList.length - 1) {
+                resolve();
+              }
+            });
+          });
+
+          advertObject["pictures"] = picsArray;
+
+          console.log("#3 advertObject", advertObject);
+
+          return advertObject;
+        } else {
+          return;
+        }
+      });
+  }
+  await browser.close();
 };
 
-scrapeList().then(async (value) => {
-    console.log('valuee', value)
-});
+scrapeList()
+  .then(async (list) => {
+    console.log("advertList", list);
+    return list;
+  })
+  .then((list) => {
+    scrapeInfoPage(list).then(() => {
+      console.log("done");
+    });
+  })
+  .catch((error) => console.log("error", error));
 
+// for (let i = 0; i < MAXPAGENUMBER; i++) {
 
+//     // const sizeLinks = await page.$$('.Button-c3851.secondary-47144.navNumberButton-d264f')
+//     // await sizeLinks[1].click({delay:1000})
 
-    // for (let i = 0; i < MAXPAGENUMBER; i++) {
+//     const [button] = await page.$x(`//button[contains(., '${i + 2}')]`);
+//     if (button) {
+//         await button.click();
+//         await page.waitForSelector('.SearchList-22b2e', { visible: true, timeout: 0 });
+//     }
 
+//     // await page.click(getFirstPageButton);
 
+//     // await page.click('.Button-c3851.secondary-47144.navNumberButton-d264f', {delay: 500});
 
-    //     // const sizeLinks = await page.$$('.Button-c3851.secondary-47144.navNumberButton-d264f')
-    //     // await sizeLinks[1].click({delay:1000}) 
+//     // await page.$eval(document.querySelector('.Button-c3851.secondary-47144.navNumberButton-d264f'), form => form.click());
 
+//     console.log(getAdvertList);
+// }
 
-    //     const [button] = await page.$x(`//button[contains(., '${i + 2}')]`);
-    //     if (button) {
-    //         await button.click();
-    //         await page.waitForSelector('.SearchList-22b2e', { visible: true, timeout: 0 });
-    //     }
+// await browser.close();
 
+// await page.click('a[href="/login"]');
 
+// await page.type('#username', "PedroTech", {delay: 100})
+// await page.type('#password', "Password123", {delay: 100})
 
+// await page.type('#username', "PedroTech")
+// await page.type('#password', "Password123")
 
-    //     // await page.click(getFirstPageButton);
+// await page.click('input[value="Login"]')
 
-    //     // await page.click('.Button-c3851.secondary-47144.navNumberButton-d264f', {delay: 500});
+// await browser.close();
 
-    //     // await page.$eval(document.querySelector('.Button-c3851.secondary-47144.navNumberButton-d264f'), form => form.click());
+// const getAdvertInfos = await page.evaluate(() => {
 
-    //     console.log(getAdvertList);
-    // }
+//     const tmp = document.querySelectorAll('.has-font-300');
 
+//     const miete = tmp[0].querySelectorAll('strong')[0];
 
+//     const flaeche = tmp[1].innerText;
 
-    // await browser.close();
+//     const zimmer = tmp[2].innerText;
 
-    // await page.click('a[href="/login"]');
+//     const objectsArray = [];
 
-    // await page.type('#username', "PedroTech", {delay: 100})
-    // await page.type('#password', "Password123", {delay: 100})
+//     page.click('https://www.immowelt.de/expose/23ree5r', {delay: 500})
 
-    // await page.type('#username', "PedroTech")
-    // await page.type('#password', "Password123")
+//     objectsArray.push({
+//         miete: miete.innerText,
+//         flaeche: flaeche,
+//         zimmer: zimmer
+//     })
 
-    // await page.click('input[value="Login"]')
-
-    // await browser.close();
-
-
-
-
-    // const getAdvertInfos = await page.evaluate(() => {
-
-    //     const tmp = document.querySelectorAll('.has-font-300');
-
-    //     const miete = tmp[0].querySelectorAll('strong')[0];
-
-    //     const flaeche = tmp[1].innerText;
-
-    //     const zimmer = tmp[2].innerText;
-
-    //     const objectsArray = [];
-
-    //     page.click('https://www.immowelt.de/expose/23ree5r', {delay: 500})
-
-    //     objectsArray.push({
-    //         miete: miete.innerText,
-    //         flaeche: flaeche,
-    //         zimmer: zimmer
-    //     })
-
-    //     return objectsArray;
-    // })
-
+//     return objectsArray;
+// })
 
 // const grapQuotes = await page.evaluate(() => {
 //     const quotes = document.querySelectorAll('.quote');
